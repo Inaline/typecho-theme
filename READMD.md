d55669a97b0f282cc9b14aa72e2933f76f68098e
Inaline <inaline@qq.com>
2025-12-30 21:03:08 +0800

    重构

---
+++ READMD.md
@@ -0,0 +1,8 @@
+# Inaline Typecho Theme
+-----------------------------
+
+## 简介
+
+功能强大、美观、简洁的 Typecho 主题
+
+https://gitee.com/inaline/typecho-theme
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * 功能强大、美观、简洁的 Typecho 主题
+ *
+ * @package Inaline Typecho Theme
+ * @author Inaline Studio
+ * @version 1.0.0
+ * @link https://gitee.com/inaline/typecho-theme
+ *
+ */
